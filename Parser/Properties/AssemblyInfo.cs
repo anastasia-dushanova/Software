89e0e7b4a5a69fff46ef96247eee4485538c3f4e
anastasia-dushanova <84286187+anastasia-dushanova@users.noreply.github.com>
2021-05-17 21:35:57 +0300

    Update AssemblyInfo.cs

--- Parser/Properties/AssemblyInfo.cs
+++ Parser/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Parser")]
-[assembly: AssemblyCopyright("Copyright ©  20")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
