5724ac7a814346d6f9cffb3f9bd5cb0c720f71aa
anastasia-dushanova <84286187+anastasia-dushanova@users.noreply.github.com>
2021-05-17 21:34:25 +0300

    Add files via upload

---
+++ Parser/ParserException.cs
@@ -0,0 +1,40 @@
+﻿using Lexer;
+using System;
+using System.Collections.Generic;
+
+namespace Parser
+{
+    public class ParserException : Exception
+    {
+        public ParserException(string message, object expected, object actual, IList<Token> listWhereProblem, int TokenIndex)
+            : base($"{message} expected: {expected}, actual: {actual}, token at {TokenIndex} = {(listWhereProblem != null && 0 <= TokenIndex && TokenIndex < listWhereProblem.Count ? listWhereProblem[TokenIndex] : null)}")
+        {
+            if(listWhereProblem != null && 0 <= TokenIndex && TokenIndex < listWhereProblem.Count)
+                this.TokenProblem = listWhereProblem[TokenIndex];
+            this.TokenIndex = TokenIndex;
+        }
+
+        public ParserException(object expected, object actual, IList<Token> listWhereProblem, int TokenIndex)
+            : base($"expected: {expected}, actual: {actual}, token at {TokenIndex} = {(listWhereProblem != null && 0 <= TokenIndex && TokenIndex < listWhereProblem.Count ? listWhereProblem[TokenIndex] : null)}")
+        {
+            if (listWhereProblem != null && 0 <= TokenIndex && TokenIndex < listWhereProblem.Count)
+                this.TokenProblem = listWhereProblem[TokenIndex];
+            this.TokenIndex = TokenIndex;
+        }
+
+        public ParserException(string messsage)
+            : base(messsage) { }
+        public ParserException()
+            : base() { }
+
+        /// <summary>
+        /// Токен, в котором возникла проблема.
+        /// </summary>
+        public Token TokenProblem { get; } = null;
+
+        /// <summary>
+        /// Идентификатор токена с проблемой.
+        /// </summary>
+        public int TokenIndex { get; } = -1;
+    }
+}
