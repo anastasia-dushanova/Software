26ecad3f6bbc6c9351011704cfaaef2ba88a2a14
anastasia-dushanova <84286187+anastasia-dushanova@users.noreply.github.com>
2021-05-22 18:14:48 +0300

    Add files via upload

---
+++ Lexer/Token.cs
@@ -0,0 +1,41 @@
+﻿namespace Lexer
+{
+    /// <summary>
+    /// Класс, представляющий токен.
+    /// </summary>
+    public class Token : Token<string>
+    {
+        public Token(Terminal Type, string Value) : base(Type, Value) { }
+    }
+
+    public class Token<T>
+    {
+        /// <summary>
+        /// Создание экземпляра токена.
+        /// </summary>
+        /// <param name="Type">Транслятор, с помощью которого
+        /// был найден токен.</param>
+        /// <param name="Value">Подстрока, которая была найдена
+        /// транслятором.</param>
+        public Token(Terminal Type, T Value)
+        {
+            this.Type = Type;
+            this.Value = Value;
+        }
+
+        /// <summary>
+        /// Тип токена.
+        /// </summary>
+        public Terminal Type { get; }
+
+        /// <summary>
+        /// Значение токена.
+        /// </summary>
+        public T Value { get; }
+
+        public override string ToString()
+        {
+            return $"{nameof(Token<T>)} {Type}: {Value}";
+        }
+    }
+}
