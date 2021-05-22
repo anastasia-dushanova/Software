6fef65bb14642aeb6eb9deb0de8ffe7b3c5a2255
anastasia-dushanova <84286187+anastasia-dushanova@users.noreply.github.com>
2021-05-22 18:26:54 +0300

    Update RuleOperator.cs

--- Parser/RuleOperator.cs
+++ Parser/RuleOperator.cs
@@ -3,8 +3,6 @@
     /// <summary>
     /// Данный класс создан для сопоставления правил
     /// нетерминалов на бумаге с программным кодом.
-    /// Чтобы посмотреть, как записываются правила нетерминалов
-    /// в оригинале, посетите страницу: <see cref="https://docs.google.com/document/d/1h-KmbeFRwl8RrXxn11LIQvDpanU4vgixCs36xMXBNUU/edit#heading=h.apcvldjdfwkn"/>
     /// </summary>
     public enum RuleOperator
     {
